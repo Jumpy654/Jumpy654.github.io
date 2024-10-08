42457ca2a9fb6b61ae193224abe1410287dde6b6
Jumpy654 <123489278+Jumpy654@users.noreply.github.com>
2024-10-08 13:37:53 -0500

    Add files via upload

---
+++ Hello world/code.js
@@ -0,0 +1,6 @@
+
+document.getElementById("b1").addEventListener("click", onclick)
+
+function onclick () {
+    document.getElementById("Conner").innerHTML = document.getElementById("Data").value 
+}
